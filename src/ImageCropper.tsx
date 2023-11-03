41f1440b9bf313521961d41bbd334a6401bd69fa
Balance.Is.Key <reimaginerei@gmail.com>
2023-11-03 15:25:06 -0400

    This moves the scan reciept function to locally to imageCropper.
    Working on getting image size and data type to appropriately send to api. Figure out a way to make crop limited to a certain files size

--- src/ImageCropper.tsx
+++ src/ImageCropper.tsx
@@ -1,13 +1,14 @@
-import React, { useState, useRef } from "react";
+import React, { useRef, useState } from "react";
 
 import ReactCrop, {
-  centerCrop,
-  makeAspectCrop,
   Crop,
   PixelCrop,
+  centerCrop,
   convertToPixelCrop,
+  makeAspectCrop,
 } from "react-image-crop";
 
+import axios from "axios";
 import "react-image-crop/dist/ReactCrop.css";
 import "./ImageCropper.css";
 import "./Scan.css";
@@ -124,24 +125,84 @@ export default function App() {
     }
   }
 
+  const OCRAPIKEY = "K82493492188957";
+
+  const scanReceipt = async (something: any) => {
+    try {
+      // Using the OCR.space default free API key (max 10reqs in 10mins) + remote file
+      //TODO: Really might have to pay for receipt scanning service. The one free OCR service has a file limit of 1024kb
+      // Mindee has a free dev for 250 scans per month 10mb limit
+      // Could have the users take our upload the photo then allow then to crop in app
+      // Could do a mix bedtween the 2 if file is larger than 1024kb go to mindee and if file is bigger than 10mb fail
+      // Intermediary caching to not have to query for the same image
+      // Cropped scan gave a better total amount and line item discovery but still not great
+      // Next task create omage preview and cropability for receipt upload
+      const body = new FormData();
+      console.log(something);
+      body.append("base64Image", something);
+      body.append("detectOrientation", "true");
+      body.append("language", "eng");
+      const config = {
+        headers: {
+          accept: "application/json",
+          apikey: OCRAPIKEY,
+          "content-type": "multipart/form-data",
+        },
+      };
+      console.log("sending request to OCR API", something);
+      let axiosResp;
+      axiosResp = await axios.post(
+        `https://api.mindee.net/v1/products/mindee/expense_receipts/v5/predict`,
+        { document: something },
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "d44b27dce5004787119f751809723882",
+          },
+        }
+      );
+      // // await axios.post(`https://api.ocr.space/parse/image`, body, config);
+      // console.log("got response from ocr API", axiosResp);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   function onDownloadCropClick() {
+    let blobType = "";
     if (!previewCanvasRef.current) {
       throw new Error("Crop canvas does not exist");
     }
-
+    console.log("previewCanvasRef", previewCanvasRef);
     previewCanvasRef.current.toBlob((blob) => {
       if (!blob) {
         throw new Error("Failed to create blob");
       }
+      console.log("oldBlob", blob);
       if (blobUrlRef.current) {
+        console.log("revoke Object url", blob);
         URL.revokeObjectURL(blobUrlRef.current);
+        console.log("blob after revoke Object Url", blob);
       }
       blobUrlRef.current = URL.createObjectURL(blob);
+      console.log("newBlob", blobUrlRef.current);
+      blobType = blob.type;
       hiddenAnchorRef.current!.href = blobUrlRef.current;
       hiddenAnchorRef.current!.click();
     });
     //TODO: Redirect with result from ocr to confirm screen with manual correction
-    window.location.assign("/home");
+    const base64ImageUrl = previewCanvasRef.current
+      .toDataURL("image/png")
+      .split(";base64,")[1];
+
+    // How to get file size from canvas  https://stackoverflow.com/questions/18557497/how-to-get-html5-canvas-todataurl-file-size-in-javascript
+    const fileSize = window.atob(
+      previewCanvasRef.current.toDataURL(blobType).split(",")[1]
+    ).length;
+    console.log("image size in bytes", fileSize);
+    console.log("image size in mb", fileSize / 1000000);
+    scanReceipt(base64ImageUrl);
+    // window.location.assign("/home");
   }
 
   useDebounceEffect(
--- src/Scan.tsx
+++ src/Scan.tsx
@@ -203,49 +203,6 @@ interface upcResponse {
   //       }
 }
 
-const OCRAPIKEY = "K82493492188957";
-
-const scanReceipt = async (something: any) => {
-  try {
-    // Using the OCR.space default free API key (max 10reqs in 10mins) + remote file
-    //TODO: Really might have to pay for receipt scanning service. The one free OCR service has a file limit of 1024kb
-    // Mindee has a free dev for 250 scans per month 10mb limit
-    // Could have the users take our upload the photo then allow then to crop in app
-    // Could do a mix bedtween the 2 if file is larger than 1024kb go to mindee and if file is bigger than 10mb fail
-    // Intermediary caching to not have to query for the same image
-    // Cropped scan gave a better total amount and line item discovery but still not great
-    // Next task create omage preview and cropability for receipt upload
-    const body = new FormData();
-    body.append("base64Image", something);
-    body.append("detectOrientation", "true");
-    body.append("language", "eng");
-    const config = {
-      headers: {
-        accept: "application/json",
-        apikey: OCRAPIKEY,
-        "content-type": "multipart/form-data",
-      },
-    };
-    // console.log("sending request to OCR API", something);
-    // let axiosResp;
-    // axiosResp = await axios.post(
-    //   `https://api.mindee.net/v1/products/mindee/expense_receipts/v5/predict`,
-    //   { document: something },
-    //   {
-    //     headers: {
-    //       Accept: "application/json",
-    //       "Content-Type": "application/json",
-    //       Authorization: "d44b27dce5004787119f751809723882",
-    //     },
-    //   }
-    // );
-    // // await axios.post(`https://api.ocr.space/parse/image`, body, config);
-    // console.log("got response from ocr API", axiosResp);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 //TODO: Need to create a the ux around the scanning. And provide a exit button
 export default function Scan() {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -356,15 +313,16 @@ export default function Scan() {
                 "Created object url from selected image",
                 URL.createObjectURL(uploadedFile)
               );
-              const fReader = new FileReader();
-              fReader.addEventListener(
-                "loadend",
-                async (reader) => {
-                  await scanReceipt(fReader.result);
-                },
-                false
-              );
-              fReader.readAsDataURL(uploadedFile);
+              // Removed to bring the scanReceipt function to image cropper
+              // const fReader = new FileReader();
+              // fReader.addEventListener(
+              //   "loadend",
+              //   async (reader) => {
+              //     await scanReceipt(fReader.result);
+              //   },
+              //   false
+              // );
+              // fReader.readAsDataURL(uploadedFile);
             }
           }}
         />
